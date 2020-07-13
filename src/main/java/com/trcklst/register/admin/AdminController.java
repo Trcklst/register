972e9ae6d69acf254698872c411d272cf2e5509b
sybernatuz <decultot.louis@gmail.com>
2020-07-13 16:48:01 +0200

    admin get all user filter by role

--- src/main/java/com/trcklst/register/admin/AdminController.java
+++ src/main/java/com/trcklst/register/admin/AdminController.java
@@ -1,6 +1,7 @@
 package com.trcklst.register.admin;
 
 import com.trcklst.register.admin.dto.UsersDto;
+import com.trcklst.register.core.db.AuthoritiesType;
 import com.trcklst.register.core.db.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class AdminController {
 
     @GetMapping("/users/")
     public UsersDto users() {
-        List<UsersDto.UserDto> users = userRepository.findAll().stream()
+        List<UsersDto.UserDto> users = userRepository.findByAuthority(AuthoritiesType.ROLE_USER).stream()
                 .map(adminMapper::mapUser)
                 .collect(Collectors.toList());
         UsersDto usersDto = new UsersDto();
--- src/main/java/com/trcklst/register/core/db/UserRepository.java
+++ src/main/java/com/trcklst/register/core/db/UserRepository.java
@@ -2,11 +2,14 @@ package com.trcklst.register.core.db;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, Integer> {
 
     Optional<User> findFirstByOrderByIdDesc();
+
+    List<User> findByAuthority(AuthoritiesType type);
 
     boolean existsByUsername(String username);
 }
